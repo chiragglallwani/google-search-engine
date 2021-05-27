a01f596883ea0d2f95ed82fe6e9495e9a1d41369
lalwanichirag3 <57692146+lalwanichirag3@users.noreply.github.com>
2021-05-27 00:55:29 -0700

    changed

--- keys.js
+++ keys.js
@@ -1,7 +1,7 @@
 //api key
 //  https://developers.google.com/custom-search/v1/using_rest
 require('dotenv').config();
-const API_KEY = "process.env.API_KEY";
+const API_KEY = process.env.API_KEY;
 
 //
 const CONTEXT_KEY = "4d174c97b8e7b443c";
